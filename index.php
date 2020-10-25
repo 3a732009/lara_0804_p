d711aa8633129a922f054d4e125d5023d5233a30
chang ting yu <s3a732009@student.ncut.edu.tw>
2020-10-26 05:10:28 +0800

    使用 Carbon 的 now()、format() 、 createFromFormat() 、diffForHumans() 等函式修改畫面上的日期時間顯示區塊

--- index.php
+++ index.php
@@ -5,6 +5,8 @@ require __DIR__.'/vendor/autoload.php';
 
 
 // connect to dabase
+use Carbon\Carbon;
+
 try {
     $dsn = 'mysql:host='.DB_HOST.';port='.DB_PORT.';dbname='.DB_DATABASE.';charset='.DB_CHARSET;
     $pdo = new PDO($dsn, DB_USERNAME, DB_PASSWORD);
@@ -83,7 +85,7 @@ try {
         <div class="row">
             <div class="col-lg-12">
                 <h1 class="page-header">文章清單
-                    <small>{今日日期}</small>
+                    <small><?php echo Carbon::now()->toDateString(); ?></small>
                 </h1>
             </div>
         </div>
@@ -103,7 +105,7 @@ try {
                 <p><?=mb_substr($row->content, 0, 130, "utf-8"),'…'?></p>
                 <p class="text-right">
                     <span class="glyphicon glyphicon-time"></span>
-                    發表於 <?=$row->created_at?>
+                    發表於 <?=Carbon::createFromFormat('Y-m-d H:i:s', $row->created_at)->diffForHumans()?>
                 </p>
             </div>
             <?php endwhile; ?>
